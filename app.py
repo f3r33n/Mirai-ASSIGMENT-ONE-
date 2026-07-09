9ed68cb8bea39ae7cd952af91b426d258116f76b
Pz Faizan <peerzadafayzan@outlook.com>
2026-07-09 11:07:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.title("ist created chatbot")
+personality=st.selectbox("who do u wantto talk to",["a Muslim imam","friendly","sarcastic","professional"], index=None , placeholder="select your personality")
+from google import genai
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+client = genai.Client(api_key =os.getenv("google_api_key"))
+user_message = st.text_input("enter youre message", value=None, placeholder = "here goes  youre message")
+if st.button("send"):
+    if not personality:
+        st.error("please select a personality")
+    if not user_message:
+        st.warning("please enter your message")
+    else:
+        prompt = f"you are a {personality} chatbot. respond to the following message: {user_message}"
+        loading_screen = st.empty()
+        loading_screen.info("generating response hang on there for a moment...")
+        
+        response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+        st.success(f"ANSWER: {response.text}")
